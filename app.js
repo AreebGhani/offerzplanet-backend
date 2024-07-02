cf58121d0a52983b2ec6e14e030b2e531c78b990
AreebGhani <areebghanidps@gmail.com>
2024-07-03 04:06:32 +0500

    updt: Server

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
 const express = require("express");
 const ErrorHandler = require("./middleware/error");
-const api = express();
+const app = express();
 const router = express.Router();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-const serverless = require("serverless-http");
-// connect db
-const connectDatabase = require("./db/Database");
-connectDatabase();
-require("dotenv").config({
-  path: ".env",
-});
 
-api.use(cors({
+app.use(cors({
   origin: 'https://offerzplanet.vercel.app',
   credentials: true
 }));
 
-api.use(express.json());
-api.use(cookieParser());
-api.use("/static", express.static(path.join(__dirname, "./uploads")));
-api.use("/", (req, res) => {
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname, "./uploads")));
+app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
-api.use('/.netlify/functions/', router);
-api.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -51,22 +44,22 @@ const brands = require("./controller/brands");
 const sponsors = require("./controller/sponsors");
 const config = require("./controller/config");
 
-api.use("/api/user", user);
-api.use("/api/conversation", conversation);
-api.use("/api/message", message);
-api.use("/api/order", order);
-api.use("/api/shop", shop);
-api.use("/api/product", product);
-api.use("/api/categories", categories);
-api.use("/api/event", event);
-api.use("/api/coupon", coupon);
-api.use("/api/payment", payment);
-api.use("/api/withdraw", withdraw);
-api.use("/api/brands", brands);
-api.use("/api/sponsors", sponsors);
-api.use("/api/config", config);
+app.use("/api/user", user);
+app.use("/api/conversation", conversation);
+app.use("/api/message", message);
+app.use("/api/order", order);
+app.use("/api/shop", shop);
+app.use("/api/product", product);
+app.use("/api/categories", categories);
+app.use("/api/event", event);
+app.use("/api/coupon", coupon);
+app.use("/api/payment", payment);
+app.use("/api/withdraw", withdraw);
+app.use("/api/brands", brands);
+app.use("/api/sponsors", sponsors);
+app.use("/api/config", config);
 
 // it's for ErrorHandling
-api.use(ErrorHandler);
+app.use(ErrorHandler);
 
-module.exports.handler = serverless(api);
+module.exports = app;
